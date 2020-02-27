ee16ad6319d006d684f621cdf931849fb70f95f0
Sahak <avdalyansahak@gmail.com>
2020-02-27 20:34:55 +0200

    Cleanup

--- src/main/kotlin/com/revolut/task/utils/GenericMessageCodec.kt
+++ src/main/kotlin/com/revolut/task/utils/GenericMessageCodec.kt
@@ -5,12 +5,14 @@ import io.vertx.core.eventbus.MessageCodec
 
 class GenericMessageCodec<T>(private val clazz: Class<T>) : MessageCodec<T, T> {
     override fun decodeFromWire(pos: Int, buffer: Buffer?): T {
+        //Not need to implement as it will be used only in clustered instance
         TODO("not implemented")
     }
 
     override fun systemCodecID(): Byte = -1
 
     override fun encodeToWire(buffer: Buffer?, s: T) {
+        //Not need to implement as it will be used only in clustered instance
         TODO("not implemented")
     }
 
